adb00b21ebc01be4b21feb53aa94028227a7aa62
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:53:06 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+//fun main(args: Array<String>) {
+ // println("Hello Afrah!")
+
+ //var name : String ="Afrah"
+ //var number : Int =8
+ //var flag : Boolean = true
+
+ //   println("the number is ${number}, my name is ${name}")
+
+// if(number > 7){
+//
+// println("the num grater than 5")
+// }else if (number >5){
+// println("the num grater than 5")
+// }
+// else{
+// println("in the else")}
+// }
+//
+// var light: String = "green"
+// if (light == "green"){
+// println("go")
+// }else if(light == "Ya")
+//
+// if (5>7)
+//
+//
+ /* val x = 20
+ val y = 18
+ if (x > y) {
+  println("x is greater than y")
+ }
+}
+*/
+//TASk
+const val APPLES =50
+ fun main(args: Array<String>) {
+  var noOfDozens = APPLES/12
+  var remainApples = APPLES%12
+
+  println(noOfDozens)
+  println(remainApples)
+ }
